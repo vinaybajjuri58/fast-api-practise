050c8d9df3dc6ef280e519fbe715680ee0feccaf
Vinay Bajjuri <vinaybajjuri58@gmail.com>
2025-04-27 16:34:27 +0530

    Feat : added fastAPI and uvicorn and implemented basic baxkend

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
 def main():
     print("Hello from fast-api-practise!")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 if __name__ == "__main__":
