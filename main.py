363a9cd262ad3e5e85679e0d30aec385948b1c44
Vinay Bajjuri <vinaybajjuri58@gmail.com>
2025-04-27 16:37:18 +0530

    Feat : removed main function to deploy

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
-def main():
-    print("Hello from fast-api-practise!")
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-if __name__ == "__main__":
-    main()
